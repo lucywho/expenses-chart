788f855329a98f3d1934a287990b784ee5c41dd6
Lucy Toman <lucy@toman.me.uk>
2022-11-17 11:41:11 +0100

    set up react  and add challenge asssets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <img src={logo} alt="logo" />
+            My balance $921.48 Spending - Last 7 days mon tue wed thu fri sat
+            sun Total this month $478.33 +2.4% from last month
+            <div class="attribution">
+                Challenge by{" "}
+                <a
+                    href="https://www.frontendmentor.io?ref=challenge"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    Frontend Mentor
+                </a>
+                . Coded by{" "}
+                <a href="https://www.frontendmentor.io/profile/lucywho">
+                    @lucywho
+                </a>
+                .
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
