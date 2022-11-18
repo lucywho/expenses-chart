a393a6d3a04cdfe32f63c8279ba1125048832c2d
Lucy Toman <lucy@toman.me.uk>
2022-11-18 16:00:10 +0100

    adds mobile styling

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,16 +1,14 @@
 export default function Summary() {
     return (
-        <>
-            <div className="summary">
-                <div className="summary-left">
-                    <p className="summary-text">Total this month</p>
-                    <p className="summary-total">$478.33</p>
-                </div>
-                <div className="summary-right">
-                    <p className="variation">+2.4%</p>
-                    <p className="summary-text">from last month</p>
-                </div>
+        <div className="summary">
+            <div className="summary-left">
+                <p className="summary-text">Total this month</p>
+                <p className="summary-total">$478.33</p>
             </div>
-        </>
+            <div className="summary-right">
+                <p className="variation">+2.4%</p>
+                <p className="summary-text">from last month</p>
+            </div>
+        </div>
     )
 }
